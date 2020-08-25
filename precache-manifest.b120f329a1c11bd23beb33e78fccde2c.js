965ee3d1c6ecdcdfc4ed00a0283acdef076784ea
stephensenft21 <stephensenft21@gmail.com>
2020-08-24 23:53:04 -0500

    Updates

---
+++ precache-manifest.b120f329a1c11bd23beb33e78fccde2c.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbe7b84deefc79c04dee51c25895b83",
+    "url": "/StephenSenft21.github.io/index.html"
+  },
+  {
+    "revision": "112860e5c5396335cc50",
+    "url": "/StephenSenft21.github.io/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5c0b606bd278a195e020",
+    "url": "/StephenSenft21.github.io/static/css/main.6c3eee0b.chunk.css"
+  },
+  {
+    "revision": "112860e5c5396335cc50",
+    "url": "/StephenSenft21.github.io/static/js/2.ca3c4a02.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/StephenSenft21.github.io/static/js/2.ca3c4a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c0b606bd278a195e020",
+    "url": "/StephenSenft21.github.io/static/js/main.0fd304fa.chunk.js"
+  },
+  {
+    "revision": "a9e1be7b5fdc914d5f61",
+    "url": "/StephenSenft21.github.io/static/js/runtime-main.d5d2fb1d.js"
+  },
+  {
+    "revision": "8e0675f7b0ef0b6d8a3e6f70c89b2402",
+    "url": "/StephenSenft21.github.io/static/media/BangazonDashboard.8e0675f7.png"
+  },
+  {
+    "revision": "52ab038bb208f3efc13ea00e41188682",
+    "url": "/StephenSenft21.github.io/static/media/BangazonFigma.52ab038b.png"
+  },
+  {
+    "revision": "f5620f05672df76dea630138b2cebb06",
+    "url": "/StephenSenft21.github.io/static/media/Casaasa.f5620f05.png"
+  },
+  {
+    "revision": "01da859161400c20bd951fe67d736a51",
+    "url": "/StephenSenft21.github.io/static/media/Employee1.01da8591.png"
+  },
+  {
+    "revision": "900b31495e3a7a2b262b1f894a1cefd7",
+    "url": "/StephenSenft21.github.io/static/media/First (4).900b3149.png"
+  },
+  {
+    "revision": "c465f1a197b82d03bd7d9f50eb3876c6",
+    "url": "/StephenSenft21.github.io/static/media/Sidekick.c465f1a1.png"
+  },
+  {
+    "revision": "0866140e1623748b2fcd8ae3f29b051d",
+    "url": "/StephenSenft21.github.io/static/media/SnowBoardProfile.0866140e.jpg"
+  },
+  {
+    "revision": "f8bc3705d4f80bc100658ca637357a6d",
+    "url": "/StephenSenft21.github.io/static/media/StephenSenftResume.f8bc3705.pdf"
+  },
+  {
+    "revision": "20cd33d11bbf1f9ac438cd17bde681f0",
+    "url": "/StephenSenft21.github.io/static/media/addComputer.20cd33d1.png"
+  },
+  {
+    "revision": "204c101ad273ad8d043e607841687800",
+    "url": "/StephenSenft21.github.io/static/media/journeyMapping.204c101a.jpg"
+  },
+  {
+    "revision": "f41549bfa7663f88fe2830939afd8df9",
+    "url": "/StephenSenft21.github.io/static/media/lofi_2.f41549bf.gif"
+  },
+  {
+    "revision": "4c6cc4903afb7ccb0803fdc6fddf7c52",
+    "url": "/StephenSenft21.github.io/static/media/logo.4c6cc490.png"
+  },
+  {
+    "revision": "92740d8645d193c61d621831dd861c25",
+    "url": "/StephenSenft21.github.io/static/media/mapping.92740d86.jpg"
+  },
+  {
+    "revision": "61e696bf736425937a9ee363ba0a5d95",
+    "url": "/StephenSenft21.github.io/static/media/me2.61e696bf.jpg"
+  },
+  {
+    "revision": "9133ede7f5b93d343226895c0bfc592e",
+    "url": "/StephenSenft21.github.io/static/media/research.9133ede7.jpg"
+  },
+  {
+    "revision": "8a2cc1cba7294a982db59e2712d43835",
+    "url": "/StephenSenft21.github.io/static/media/website35.8a2cc1cb.png"
+  }
+]);
